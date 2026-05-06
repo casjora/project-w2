4247d0f4ab52aaa6e68bea14b66fdacfd35e6e5a
casjora <casjora@gmail.com>
2026-05-05 22:08:15 -0600

    commit 2 js

---
+++ scripts/scripts.js
@@ -0,0 +1,26 @@
+const imagen = document.getElementById('hero-img');
+
+const images = [
+    './images/me-1-bg.png',
+    './images/me-2-bg.png',
+    './images/me-3-bg.png',
+    './images/me-4-bg.png',
+    './images/me-5-bg.png'
+];
+
+let index=0;
+
+
+function cambiarImagen(){
+    imagen.style.opacity='0';
+    setTimeout(()=> {
+        index = (index + 1) % images.length;
+        imagen.src=images[index];
+
+        imagen.style.opacity ='1';
+
+    },1000)
+
+}
+
+setInterval(cambiarImagen,5000);
